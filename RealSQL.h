98df94a816e54aecc2b83e4efb8e160b0738efed
Andy Lu <37600885+Andyluchina@users.noreply.github.com>
2018-11-22 05:19:40 -0500

    extra credit

--- RealSQL.h
+++ RealSQL.h
@@ -6,6 +6,40 @@
 #define UNTITLED_REALSQL_H
 
 #endif //UNTITLED_REALSQL_H
+#include <stdio.h>
+#include <malloc.h>
+#include <string.h>
+#include <stdbool.h>
+
+
+typedef struct attributes{
+    char * attr;
+    struct attitubes * next;
+}attributes;
+
+
+attributes * attrInit(char * attr){
+    attributes * a = malloc(sizeof(attributes));
+    a->attr=attr;
+    a->next=NULL;
+}
+
+void appendAttribute(attributes * head, char * attr){
+  //  attributes * temp = head;
+    while(head->next!=NULL){
+        head=head->next;
+    }
+    attributes * data = attrInit(attr);
+    head->next=data;
+};
+
+void printAttributes(attributes * attr){
+    do{
+        printf("%s  ",attr->attr);
+        attr=attr->next;
+    }while(attr!=NULL);
+    printf("\n");
+};
 
 typedef struct element{
     char name[50];
@@ -21,8 +55,73 @@ typedef struct node{
 }node;
 
 typedef struct Table{
-    char name[50];
+    char * name;
     //array of strings
-    char attributs[50][50];
+    struct attributes * attributes;
     node * data[1009];
+    struct Table * nextTable;
 }Table;
+
+typedef struct RealSQL{
+    Table * root;
+}RealSQL;
+
+
+//initialize the SQL
+RealSQL * sqlinit(){
+    RealSQL * s = malloc(sizeof(RealSQL));
+    //load the data
+    s->root=NULL;
+    return s;
+}
+
+bool tableExist(RealSQL * SQL, char * name) {
+
+    if (SQL->root == NULL) {
+        return 0;
+    } else {
+        Table *temp = SQL->root;
+         do{
+            if (strcmp(name, temp->name) == 0) {
+                return 1;
+            }
+            temp = temp->nextTable;
+        }while (temp != NULL);
+         return 0;
+        //   temp->nextTable=t;
+    }
+};
+//create table
+bool createTable(RealSQL * SQL, char * name, attributes * attributes){
+    if(tableExist(SQL,name)==1){
+        printf("Existed");
+        return 0;
+    }
+    Table * t = malloc(sizeof(Table));
+    t->name=name;
+    t->attributes=attributes;
+    t->nextTable=NULL;
+    for (int i = 0; i < 1009; i++) {
+        t->data[i]=malloc(sizeof(node));
+    }
+    printf("created\n");
+    if(SQL->root==NULL){
+        SQL->root=t;
+    }else{
+       // Table * temp=SQL->root;
+        while(SQL->root->nextTable!=NULL){
+            SQL->root=SQL->root->nextTable;
+        }
+        SQL->root->nextTable=t;
+        printf("done\n");
+    }
+
+    return 1;
+};
+
+
+
+//insert into the table
+bool insertIntoTable(RealSQL * SQL, char * tableName, node * data){
+
+};
--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "database.h"
+#include "RealSQL.h"
 
 
 void REPL(){
@@ -49,130 +50,153 @@ void REPL2(){
 }
 
 int main() {
-    TUPLELISTSNAP HTSNAP = malloc(sizeof (struct SNAP) * 1009);
-    printf("NOW SHOWING S-N-A-P relation: \n");
+//    TUPLELISTSNAP HTSNAP = malloc(sizeof (struct SNAP) * 1009);
+//    printf("NOW SHOWING S-N-A-P relation: \n");
+//
+//    insert_SNAP(12345, "C. Brown", "12 Apple St.", "555-1234");
+//    insert_SNAP(67890, "L. Van Pelt", "34 Pear Ave.", "555-5678");
+//    insert_SNAP(22222, "P. Patty", "56 Grape Blvd.", "555-9999");
+//
+//    printSNAP();
+//    printf("\nlookup function on 0, \"*\", \"12 Apple St.\", \"*\": \n");
+//    TUPLELISTSNAP matching = lookup_SNAP(0, "*", "12 Apple St.", "*");
+//    printSNAPTUPLEList(matching);
+//
+//    free(HTSNAP);
+//    printf("\n\n");
+//
+//    printf("NOW SHOWING C-S-G relation: \n");
+//
+//    TUPLELISTCSG HTCSG = malloc(sizeof (TUPLELISTCSG) * 128);
+//    insert_CSG("CS101", 12345, "A");
+//    insert_CSG("CS101", 67890, "B");
+//    insert_CSG("EE200", 12345, "C");
+//    insert_CSG("EE200", 22222, "B+");
+//    insert_CSG("CS101", 33333, "A-");
+//    insert_CSG("PH100", 67890, "C+");
+//    insert_CSG("CS101", 33333, "A-");
+//    printCSG();
+//    printf("\nlookup function on \"CS101\", 12345, \"*\": \n");
+//    TUPLELISTCSG matching5 = lookup_CSG("CS101", 12345, "*");
+//    printCSGTUPLEList(matching5);
+//
+//    printf("\n\n");
+//    printf("NOW SHOWING C-P relation: \n");
+//
+//    TUPLELISTCP HTCP = malloc(sizeof (TUPLELISTCP) * 128);
+//    insert_CP("CS101", "CS100");
+//    insert_CP("EE200", "EE005");
+//    insert_CP("EE200", "CS100");
+//    insert_CP("CS120", "CS101");
+//    insert_CP("CS121", "CS120");
+//    insert_CP("CS205", "CS101");
+//    insert_CP("CS206", "CS121");
+//    insert_CP("CS206", "CS205");
+//
+//    printCP();
+//
+//    //TESTING FROM EXAMPLE 8.2!!!
+//
+//    printf("\nlookup function on \"CS205\", \"CS120\": \n");
+//    TUPLELISTCP textbookTest = lookup_CP("CS205", "CS120");
+//    printCPTUPLEList(textbookTest);
+//
+//    insert_CP("CS205", "CS120");
+//    insert_CP("CS205", "CS101");
+//
+//    printf("lookup function on \"CS205\", \"CS120\" AFTER INSERTING IT: \n");
+//    TUPLELISTCP textbookTest2 = lookup_CP("CS205", "CS120");
+//    printCPTUPLEList(textbookTest2);
+//
+//    //TESTING '*' CAPABILITY
+//    printf("\nlookup function on \"CS206\", \"*\": \n");
+//    TUPLELISTCP matching4 = lookup_CP("CS206", "*");
+//    printCPTUPLEList(matching4);
+//        printf("\n\n");
+//    printf("NOW SHOWING C-D-H relation: \n");
+//
+//
+//    //CONCANTENATION ISSUE WHICH CAN'T BE SOLVED WITH THE ABOVE METHOD
+//    TUPLELISTCDH HTCDH = malloc(sizeof (TUPLELISTCDH) * 128);
+//    insert_CDH("CS101", "M", "9AM");
+//    insert_CDH("CS101", "W", "9AM");
+//    insert_CDH("CS101", "F", "9AM");
+//    insert_CDH("EE200", "Tu", "10AM");
+//    insert_CDH("EE200", "W", "1PM");
+//    insert_CDH("EE200", "Th", "10AM");
+//    insert_CDH("EE200", "Th", "10AM");
+//    printCDH();
+//    printf("\nlookup function of *, *, '9AM' : \n");
+//    TUPLELISTCDH matching3 = lookup_CDH("*", "*", "9AM");
+//    printCDHTUPLEList(matching3);
+//
+//    printf("\n\n");
+//    printf("NOW SHOWING C-R relation: \n");
+//    TUPLELISTCR HTCR = malloc(sizeof (TUPLELISTCR) * 128);
+//    insert_CR("CS101", "Turing Aud.");
+//    insert_CR("EE200", "25 Ohm Hall");
+//    insert_CR("PH100", "Newton Lab.");
+//    insert_CR("PH100", "Newton Lab.");
+//    printCR();
+//
+//    printf("\nlookup function of \"CS101\", \"Turing Aud.\": \n");
+//    TUPLELISTCR matching2 = lookup_CR("CS101", "Turing Aud.");
+//    printCRTUPLEList(matching2);
+//
+//    printf("Deleting \"CS101\", \"Turing Aud.\" and printing remaining: \n");
+//    delete_CR("CS101", "Turing Aud.");
+//    printCR();
+//
+//    insert_CR("CS101", "Turing Aud."); //ADDING BACK FOR PART 2
+//
+//
+//    //PART TWO!!!
+//    printf("\n\n\n NOW TESTING PART TWO REPL \n");
+////    REPL();
+////    REPL2();
+//
+//    //PART THREE!!!!!
+//    printf("\n\n NOW TESTING PART THREE \n");
+//    printf("printing SELECT course='CS101' on CSG: \n");
+//    TUPLELISTCSG selectedTuples = select_CSG("CS101", 0, "*");
+//    printCSGTUPLEList(selectedTuples);
+//
+//    printf("printing PROJECT 'studentID' of SELECT course='CS101' of CSG:\n");
+//    TUPLELISTid id = project_CSG(selectedTuples);
+//    printID(id);
+//
+//    TUPLELISTCR cr = lookup_CR("*", "*");
+//    TUPLELISTCDH cdh = lookup_CDH("*", "*", "*");
+//    printf("printing JOIN of CR and CDH on Course:\n");
+//    TUPLELISTCRDH crdh = join_CR_CDH(cr, cdh);
+//    printCRDHTUPLELIST(crdh);
+//
+//    printf("printing PROJECT 'day, hour' of SELECT 'Turing Aud.' of JOIN of CR and CDH on Course: \n");
+//    TUPLELISTCRDH crdh2 = select_CRDH("Turing Aud.", crdh);
+//    TUPLELISTDH dh = project_CRDH(crdh2);
+//    printDH(dh);
+//    return 0;
 
-    insert_SNAP(12345, "C. Brown", "12 Apple St.", "555-1234");
-    insert_SNAP(67890, "L. Van Pelt", "34 Pear Ave.", "555-5678");
-    insert_SNAP(22222, "P. Patty", "56 Grape Blvd.", "555-9999");
+    RealSQL * SQL= sqlinit();
+    //printf("%s", SQL->root);
+    attributes * a = attrInit("studentid");
+    appendAttribute(a, "name");
+    appendAttribute(a, "room");
+    appendAttribute(a, "grade");
+    //printAttributes(a);
+    createTable(SQL,"Students",a);
+    printf("%s\n",SQL->root->name);
+    printAttributes(SQL->root->attributes);
 
-    printSNAP();
-    printf("\nlookup function on 0, \"*\", \"12 Apple St.\", \"*\": \n");
-    TUPLELISTSNAP matching = lookup_SNAP(0, "*", "12 Apple St.", "*");
-    printSNAPTUPLEList(matching);
+    attributes * b = attrInit("fuckid");
+    appendAttribute(b, "name");
+    appendAttribute(b, "room");
+    appendAttribute(b, "grade");
+    createTable(SQL,"fuck",a);
+    printf("%s\n",SQL->root->nextTable->name);
+    printAttributes(SQL->root->nextTable->attributes);
 
-    free(HTSNAP);
-    printf("\n\n");
-
-    printf("NOW SHOWING C-S-G relation: \n");
-
-    TUPLELISTCSG HTCSG = malloc(sizeof (TUPLELISTCSG) * 128);
-    insert_CSG("CS101", 12345, "A");
-    insert_CSG("CS101", 67890, "B");
-    insert_CSG("EE200", 12345, "C");
-    insert_CSG("EE200", 22222, "B+");
-    insert_CSG("CS101", 33333, "A-");
-    insert_CSG("PH100", 67890, "C+");
-    insert_CSG("CS101", 33333, "A-");
-    printCSG();
-    printf("\nlookup function on \"CS101\", 12345, \"*\": \n");
-    TUPLELISTCSG matching5 = lookup_CSG("CS101", 12345, "*");
-    printCSGTUPLEList(matching5);
-
-    printf("\n\n");
-    printf("NOW SHOWING C-P relation: \n");
-    
-    TUPLELISTCP HTCP = malloc(sizeof (TUPLELISTCP) * 128);
-    insert_CP("CS101", "CS100");
-    insert_CP("EE200", "EE005");
-    insert_CP("EE200", "CS100");
-    insert_CP("CS120", "CS101");
-    insert_CP("CS121", "CS120");
-    insert_CP("CS205", "CS101");
-    insert_CP("CS206", "CS121");
-    insert_CP("CS206", "CS205");
-
-    printCP();
-
-    //TESTING FROM EXAMPLE 8.2!!!
-
-    printf("\nlookup function on \"CS205\", \"CS120\": \n");
-    TUPLELISTCP textbookTest = lookup_CP("CS205", "CS120");
-    printCPTUPLEList(textbookTest);
-
-    insert_CP("CS205", "CS120");
-    insert_CP("CS205", "CS101");
-
-    printf("lookup function on \"CS205\", \"CS120\" AFTER INSERTING IT: \n");
-    TUPLELISTCP textbookTest2 = lookup_CP("CS205", "CS120");
-    printCPTUPLEList(textbookTest2);
-
-    //TESTING '*' CAPABILITY
-    printf("\nlookup function on \"CS206\", \"*\": \n");
-    TUPLELISTCP matching4 = lookup_CP("CS206", "*");
-    printCPTUPLEList(matching4);
-        printf("\n\n");
-    printf("NOW SHOWING C-D-H relation: \n");
-
-
-    //CONCANTENATION ISSUE WHICH CAN'T BE SOLVED WITH THE ABOVE METHOD
-    TUPLELISTCDH HTCDH = malloc(sizeof (TUPLELISTCDH) * 128);
-    insert_CDH("CS101", "M", "9AM");
-    insert_CDH("CS101", "W", "9AM");
-    insert_CDH("CS101", "F", "9AM");
-    insert_CDH("EE200", "Tu", "10AM");
-    insert_CDH("EE200", "W", "1PM");
-    insert_CDH("EE200", "Th", "10AM");
-    insert_CDH("EE200", "Th", "10AM");
-    printCDH();
-    printf("\nlookup function of *, *, '9AM' : \n");
-    TUPLELISTCDH matching3 = lookup_CDH("*", "*", "9AM");
-    printCDHTUPLEList(matching3);
-
-    printf("\n\n");
-    printf("NOW SHOWING C-R relation: \n");
-    TUPLELISTCR HTCR = malloc(sizeof (TUPLELISTCR) * 128);
-    insert_CR("CS101", "Turing Aud.");
-    insert_CR("EE200", "25 Ohm Hall");
-    insert_CR("PH100", "Newton Lab.");
-    insert_CR("PH100", "Newton Lab.");
-    printCR();
-
-    printf("\nlookup function of \"CS101\", \"Turing Aud.\": \n");
-    TUPLELISTCR matching2 = lookup_CR("CS101", "Turing Aud.");
-    printCRTUPLEList(matching2);
-
-    printf("Deleting \"CS101\", \"Turing Aud.\" and printing remaining: \n");
-    delete_CR("CS101", "Turing Aud.");
-    printCR();
-
-    insert_CR("CS101", "Turing Aud."); //ADDING BACK FOR PART 2
-
-
-    //PART TWO!!!
-    printf("\n\n\n NOW TESTING PART TWO REPL \n");
-//    REPL();
-//    REPL2();
-
-    //PART THREE!!!!!
-    printf("\n\n NOW TESTING PART THREE \n");
-    printf("printing SELECT course='CS101' on CSG: \n");
-    TUPLELISTCSG selectedTuples = select_CSG("CS101", 0, "*");
-    printCSGTUPLEList(selectedTuples);
-
-    printf("printing PROJECT 'studentID' of SELECT course='CS101' of CSG:\n");
-    TUPLELISTid id = project_CSG(selectedTuples);
-    printID(id);
-
-    TUPLELISTCR cr = lookup_CR("*", "*");
-    TUPLELISTCDH cdh = lookup_CDH("*", "*", "*");
-    printf("printing JOIN of CR and CDH on Course:\n");
-    TUPLELISTCRDH crdh = join_CR_CDH(cr, cdh);
-    printCRDHTUPLELIST(crdh);
-
-    printf("printing PROJECT 'day, hour' of SELECT 'Turing Aud.' of JOIN of CR and CDH on Course: \n");
-    TUPLELISTCRDH crdh2 = select_CRDH("Turing Aud.", crdh);
-    TUPLELISTDH dh = project_CRDH(crdh2);
-    printDH(dh);
-    return 0;
+    createTable(SQL,"Ssdafds",b);
+//    printf("%s\n",SQL->root->nextTable->nextTable->name);
+//    printAttributes(SQL->root->nextTable->nextTable->attributes);
 }
