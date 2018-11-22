4821141c261ad6d17e302b873008d4932b5e676c
Andy Lu <37600885+Andyluchina@users.noreply.github.com>
2018-11-22 02:36:09 -0500

    extra credit

---
+++ RealSQL.h
@@ -0,0 +1,18 @@
+//
+// Created by Andy on 11/22/2018.
+//
+
+#ifndef UNTITLED_REALSQL_H
+#define UNTITLED_REALSQL_H
+
+#endif //UNTITLED_REALSQL_H
+
+typedef struct element{
+    char * name;
+    char * value;
+}element;
+
+//a entity of data
+typedef struct node{
+
+};
