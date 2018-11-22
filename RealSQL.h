1707b154abb046c3c1fbbe7d4605731010f277c3
Andy Lu <37600885+Andyluchina@users.noreply.github.com>
2018-11-22 02:44:50 -0500

    extra credit

--- RealSQL.h
+++ RealSQL.h
@@ -8,11 +8,21 @@
 #endif //UNTITLED_REALSQL_H
 
 typedef struct element{
-    char * name;
-    char * value;
+    char name[50];
+    char value[50];
+    struct element * nextElement;
 }element;
 
 //a entity of data
 typedef struct node{
+    int hashId;
+    struct element head;
+    struct node * nextNode;
+}node;
 
-};
+typedef struct Table{
+    char name[50];
+    //array of strings
+    char attributs[50][50];
+    node * data[1009];
+}Table;
